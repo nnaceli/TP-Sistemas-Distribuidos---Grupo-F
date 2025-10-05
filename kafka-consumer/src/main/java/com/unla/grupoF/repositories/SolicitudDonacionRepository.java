b9d3d77b0e3d9dbe9869f74252a5d0bee44bfd10
caro <caroaguirre999@gmail.com>
2025-10-05 17:15:22 -0300

    solicitud de donacion

---
+++ kafka-consumer/src/main/java/com/unla/grupoF/repositories/SolicitudDonacionRepository.java
@@ -0,0 +1,9 @@
+package com.unla.grupoF.repositories;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.unla.grupoF.entities.SolicitudDonacion;
+
+public interface SolicitudDonacionRepository extends JpaRepository<SolicitudDonacion, Long> {
+    Optional<SolicitudDonacion> findBySolicitudId(String solicitudId);
+}
